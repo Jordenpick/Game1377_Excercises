c2b6a684408df6cfee03954b90ea97356427ae20
jordenpick <jorde@LAPTOP-MDA8JB3V>
2025-02-12 19:10:42 -0600

    For the Switch(), understanding how to use a switch statement with user input and key controls. For the Arrays(), learning how to output the integers in reverse order/ in order. Using for/while/foreach loops. Finding the average in a array using a for loop. Using a while loop to find the correct number in the array to match the users input. Using user input to store in multiple numbers and dynamically creating a array of it. As well as using random numbers. For the rest of the methods, using a method to output all of the elements in a array, using overloaded methods that can output floats/doubles.  Writing methods that can find the sum of a array as well as the sum of the overloaded methods.

---
+++ Exercise 4/Program.cs
@@ -0,0 +1,201 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata.Ecma335;
+using System.Transactions;
+
+namespace Exercise_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Switch();
+            Arrays();
+        }
+
+        static void Switch()
+        {
+            Console.WriteLine("Input a WASD key.");
+            ConsoleKeyInfo inputKey = Console.ReadKey();
+            int input = (char)Console.ReadKey().Key;
+            Console.WriteLine();
+
+            if (inputKey.Key == ConsoleKey.W)
+            {
+                input = 0;
+            }
+            else if (inputKey.Key == ConsoleKey.A)
+            {
+                input = 1;
+            }
+            else if (inputKey.Key == ConsoleKey.S)
+            {
+                input = 2;
+            }
+            else if (inputKey.Key == ConsoleKey.D)
+            {
+                input = 3;
+            }
+            else
+            {
+                input = default;
+            }
+            switch (input)
+            {
+                case 0:
+                    Console.WriteLine("You moved up");
+                    break;
+                case 1:
+                    Console.WriteLine("You moved down");
+                    break;
+                case 2:
+                    Console.WriteLine("You moved left");
+                    break;
+                case 3:
+                    Console.WriteLine("You moved right");
+                    break;
+                default:
+                    Console.WriteLine("You did not press a WASD key");
+                    break;
+            }
+            Console.WriteLine();
+        }
+        static void Arrays()
+        {
+            int[] numbers = { 100, 94, 159, -783, 132, 179, 47, 107, 135, 50 };
+            for (int i = numbers.Length; i > 0; i--)
+            {
+
+                Console.WriteLine(numbers[i - 1]);
+
+            }
+            bool numFound = false;
+
+            while (!numFound)
+            {
+                Console.WriteLine("Type an input number.");
+                string? result = Console.ReadLine();
+                int.TryParse(result, out int userNumber);
+                foreach (int number in numbers)
+                {
+                    if (number == userNumber)
+                    {
+                        Console.WriteLine("You found a correct input");
+                        numFound = true;
+                        break;
+                    }
+
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine("The sum of the input numbers is " + sum);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(numbers[i]);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 3 != 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("Enter your first number");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number");
+            int rangeMin = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your third number");
+            int rangeMax = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[arraySize];
+            Random rmd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rmd.Next(rangeMin, rangeMax);
+            }
+        }
+        private static void IntegerArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+        private static void OverLoadMethods(float[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+        private static void OverLoadMethods(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+        private static void IntegerIntoFloat(int[] array)
+        {
+            float sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += (float)array[i];
+            }
+        }
+        private static void AverageFloat(float[] array)
+        {
+            float sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+        }
+        private static void AverageDouble(double[] array)
+        {
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+        }
+        private static void RandomInputDynamics(int[] array)
+        {
+            Console.WriteLine("Enter your first number");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number");
+            int rangeMin = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your third number");
+            int rangeMax = Convert.ToInt32(Console.ReadLine());
+
+            array = new int[arraySize];
+            Random rmd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rmd.Next(rangeMin, rangeMax);
+            }
+        }
+    }
+}
